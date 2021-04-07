8d691d73ee52b856c2f567de8b9d8a0bac754c88
Maria <denisastreinu@gmail.com>
2021-04-07 15:55:08 +0100

    Added fortuneteller.js

---
+++ fortuneteller.js
@@ -0,0 +1,8 @@
+let randomNum = x => {
+    return Math.floor(Math.random() * x)
+};
+let messages = {
+    element: ['earth', 'water', 'air', 'fire']
+    avoid: ['cozy parks', 'meeting strangers', 'nights out', 'birthday parties']
+    do: ['spend time with loved ones', 'spend time alone', 'trust no one', 'release energy']
+}
