909fbb7d2e996c19d419cb7a1e75e6031d51fb57
Maria <denisastreinu@gmail.com>
2021-04-07 16:01:10 +0100

    added all files

--- fortuneteller.js
+++ fortuneteller.js
@@ -5,4 +5,5 @@ let messages = {
     element: ['earth', 'water', 'air', 'fire']
     avoid: ['cozy parks', 'meeting strangers', 'nights out', 'birthday parties']
     do: ['spend time with loved ones', 'spend time alone', 'trust no one', 'release energy']
-}
+};
+let 
